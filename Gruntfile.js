f078b80341c8962e32f4cd76fd5dcdef026647be
Andre Henn <henn@terrestris.de>
2016-06-07 11:48:08 +0200

    Updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,3 @@
-'use strict';
-
 module.exports = function (grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-clean');
@@ -8,16 +6,51 @@ module.exports = function (grunt) {
 	grunt.initConfig({
 		'gh-pages': {
 			options: {
-				branch: 'gh-pages',
-        repo: 'https://github.com/ahennr/SolrHeatmap.git',
-        message: 'Publish gh-pages (grunt cli)'
+				branch: 'gh-pages'
 			},
-      src: ['**/*']
+      publish: {
+        repo: 'https://github.com/ahennr/SolrHeatmap.git',
+        message: 'Publish gh-pages (grunt cli)',
+        src: ['**/*']
+      },
+			deploy: {
+				options: {
+					user: {
+						name: 'ahennr',
+						email: 'henn@terrestris.de'
+					},
+					repo: 'https://' + process.env.GH_TOKEN + '@github.com/terrestris/SolrHeatmap.git',
+					message: 'Publish gh-pages (auto)' + getDeployMessage(),
+					silent: true
+				},
+				src: '**/*'
+			}
+
 		}
 	});
 
+  // get a formatted commit message to review changes from the commit log
+	// github will turn some of these into clickable links
+	function getDeployMessage() {
+		var ret = '\n\n';
+		if (process.env.TRAVIS !== 'true') {
+			ret += 'missing env vars for travis-ci';
+			return ret;
+		}
+		ret += 'branch:       ' + process.env.TRAVIS_BRANCH + '\n';
+		ret += 'SHA:          ' + process.env.TRAVIS_COMMIT + '\n';
+		ret += 'range SHA:    ' + process.env.TRAVIS_COMMIT_RANGE + '\n';
+		ret += 'build id:     ' + process.env.TRAVIS_BUILD_ID  + '\n';
+		ret += 'build number: ' + process.env.TRAVIS_BUILD_NUMBER + '\n';
+		return ret;
+	}
+
 	grunt.registerTask('build', 'SolrHeatmap deploy to gh-pages', [
-		'gh-pages:gh-pages'
+		'gh-pages:deploy'
+	]);
+
+  grunt.registerTask('publish', 'Publish from CLI', [
+		'gh-pages:publish'
 	]);
 
 };
